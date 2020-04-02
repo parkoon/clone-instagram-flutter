537b77843e31ab6877d174ce56a567825df2f278
parkoon <jh.park@qualson.com>
2020-04-02 18:04:18 +0900

    init & add home page

---
+++ lib/root_page.dart
@@ -0,0 +1,9 @@
+import 'package:com/tab_page.dart';
+import 'package:flutter/material.dart';
+
+class RootPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return TabPage();
+  }
+}
---
+++ lib/tab_page.dart
@@ -0,0 +1,42 @@
+import 'package:com/home_page.dart';
+import 'package:flutter/material.dart';
+
+class TabPage extends StatefulWidget {
+  @override
+  _TabPageState createState() => _TabPageState();
+}
+
+class _TabPageState extends State<TabPage> {
+  int _selectedIndex = 0;
+
+  List _pages = [
+    HomePage(),
+    Text('page2'),
+    Text('page3'),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _pages[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        fixedColor: Colors.black,
+        onTap: _onItemTapped,
+        currentIndex: _selectedIndex,
+        items: <BottomNavigationBarItem>[
+          BottomNavigationBarItem(icon: Icon(Icons.home), title: Text('Home')),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.search), title: Text('Search')),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.account_circle), title: Text('Account')),
+        ],
+      ),
+    );
+  }
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+}
